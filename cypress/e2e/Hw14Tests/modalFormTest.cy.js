ab68a0dab2b8929865e523c53384084a33a0bb50
MTV <matveimtvcool@gmai.com>
2024-03-08 15:59:54 +0200

    HW_014 Done refactoring

--- cypress/e2e/Hw14Tests/modalFormTest.cy.js
+++ cypress/e2e/Hw14Tests/modalFormTest.cy.js
@@ -1,13 +1,4 @@
-describe("Test different cases", () => {
-  it("checkStepsTest", () => {
-    cy.visit("pages/layout/stepper")
-    cy.get("nb-card.col-md-12").as("stepper")
-    cy.get('@stepper').find("h3.ng-star-inserted").should('have.text', 'Step content #1')
-    for (let i = 2; i <= 4; i ++) {
-      cy.get("@stepper").find("button").last().click()
-      cy.get('@stepper').find("h3.ng-star-inserted").should('have.text', `Step content #${i}`)
-    }
-  })
+describe("Modal Form Tests", () => {
   it("checkModalFormTest", () => {
     cy.visit("pages/modal-overlays/dialog")
     cy.get("nb-card.form-input-card").as("returnResultFromDialog")
