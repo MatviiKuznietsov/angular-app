34a0b8875eec6558993496a4a2b952c0530fdd13
MTV <matveimtvcool@gmai.com>
2024-03-08 12:05:28 +0200

    HW_014 Done

--- cypress/e2e/Hw14Tests/hw14Tests.cy.js
+++ cypress/e2e/Hw14Tests/hw14Tests.cy.js
@@ -3,7 +3,7 @@ describe("Hw 014 test 1", () => {
     cy.visit("pages/layout/stepper")
     cy.get("nb-card.col-md-12").as("stepper")
     cy.get('@stepper').find("h3.ng-star-inserted").should('have.text', 'Step content #1')
-    for(let i=2; i<=4; i++){
+    for (let i = 2; i <= 4; i ++) {
       cy.get("@stepper").find("button").last().click()
       cy.get('@stepper').find("h3.ng-star-inserted").should('have.text', `Step content #${i}`)
     }
@@ -19,21 +19,7 @@ describe("Hw 014 test 1", () => {
     cy.get('@modalWindow').find("button").last().should('be.visible')
   })
 })
-// describe("Hw 014 test 2", () => {
-//   before("Preparation", ()=>{
-//     cy.visit("pages/modal-overlays/dialog")
-//   })
-//   it("Test2", () => {
-//     cy.get("nb-card.form-input-card").as("returnResultFromDialog")
-//     cy.get('@returnResultFromDialog').find("button").click();
-//
-//     cy.get("nb-dialog-container").as("modalWindow")
-//     cy.get('@modalWindow').find("nb-card-header").should('have.text', 'Enter your name')
-//     cy.get('@modalWindow').find("input").should('be.visible')
-//     cy.get('@modalWindow').find("button").first().should('be.visible')
-//     cy.get('@modalWindow').find("button").last().should('be.visible')
-//   })
-// })
+
 
 
 
